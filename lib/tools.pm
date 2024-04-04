5a10b4509177db45799440d37e022260065b0cf6
VADIM <v1999.77@gmail.com>
2024-04-04 16:48:29 +0300

    task_10 && task_9

--- lib/tools.pm
+++ lib/tools.pm
@@ -67,8 +67,8 @@ sub reg_usr {
 sub get_params_from_env {
 
     my ($user_name, $user_passwd, $action) = @ENV{ qw /user_name user_passwd action/ };
-    unless (defined $user_name && defined $user_passwd) {
-        die "Пожалуйста добавьте параметры user_name, user_passwd, action в ENV \n";
+    unless (defined $user_name && defined $action) {
+        die "Пожалуйста добавьте параметры user_name, user_passwd (optional), action в ENV \n";
     }
     return ($user_name, $user_passwd, $action);
 }
@@ -85,6 +85,64 @@ sub rewrite_config {
 
 
     close $fh;
+}
+
+sub check_user_name {
+    my ($user_name) = @_;
+
+    # Проверка логина
+    if ($user_name =~ m/^[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]$/) {
+        return 1;
+    } else {
+        say
+        "Логин невалиден: должен начинаться с буквы, может содержать цифры, тире, нижнее
+        подчеркивание, заканчиваться на цифру или букву,
+        не содержать кириллицу.";
+        return 0;
+    }
+}
+
+
+sub check_user_passwd {
+    my ($password) = @_;
+
+    unless (length($password) >= 8) {
+        say 'Пароль должен быть не менее 8 символов';
+        return 0;
+    }
+    unless ($password =~ m/^[a-zA-Z]/) {
+        say 'Пароль должен начинаться с латинской буквы';
+        return 0;
+    }
+    unless ($password =~ m/[!@#\$^%&*()]/) {
+        say 'Пароль должен содержать минимум один спецсимвол из списка !@#$\%^&*()';
+        return 0;
+    }
+    unless ($password =~ m/[A-Z]/) {
+        say 'Пароль должен содержать минимум один символ в верхнем регистре';
+        return 0;
+    }
+    unless ($password =~ m/\d/) {
+        say 'Пароль должен содержать минимум одну цифру';
+        return 0;
+    }
+
+    return 1;
+}
+
+
+sub delete_user {
+    my $user_name = shift;
+    my %config = read_conf;
+    if (exists $config{$user_name}) {
+        delete $config{$user_name};
+        rewrite_config \%config;
+        say "Пользователь $user_name был успешно удален!";
+        return 1;
+    }
+
+    say "Пользователь не был удален т.к. он не существовал в базе!\n";
+    return 0;
 }
 
 1;
